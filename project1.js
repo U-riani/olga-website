3d076ce1c3ad2aa98707158b13ccb611d4bdf3d0
U-riani <sand.papi@yahoo.com>
2023-11-03 19:05:35 +0400

    third

--- project1.js
+++ project1.js
@@ -2,7 +2,7 @@ import { App } from "./renderImages.js";
 import { imagePopup } from "./imagePopup.js";
 
 imagePopup();
-const project1 = new App('imagesCopmress-1');
+const project1 = new App('imagesCompress-1');
 project1.loadImagesMobile(0);
 project1.loadImagesPad(0);
 project1.loadImagesComp(0);
--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@ import {App} from './renderImages.js';
 const projectsPage = new App();
 
 document.querySelectorAll('.photo-container').forEach(function(el, i) {
-    projectsPage.createPhotos(el, `/images/${i + 1}.jpg`);
+    projectsPage.createPhotos(el, `/imagesCompress-${i + 1}/${1}.jpg`);
 })
 
 document.querySelectorAll('.projects').forEach(function(e) {
