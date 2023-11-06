e30f2e463dc681c6dbe5ebe6cfdefd938403d851
U-riani <sand.papi@yahoo.com>
2023-11-06 19:03:11 +0400

    sixth

---
+++ project4.js
@@ -0,0 +1,9 @@
+import { App } from "./renderImages.js";
+import { imagePopup } from "./imagePopup.js";
+
+imagePopup();
+const project1 = new App('imagesCompress-4');
+project1.loadImagesMobile();
+project1.loadImagesPad();
+project1.loadImagesComp();
+project1.revealNavMenu();
